c691d56b05b1879c0f88fc1e545f281c65762ced
Jaime <jaime.perez@uc.cl>
2022-05-16 16:11:22 -0400

    Foreign Keys restrictions implementadas

--- schema.sql
+++ schema.sql
@@ -1,23 +1,23 @@
-DROP TABLE IF EXISTS persona;
-DROP TABLE IF EXISTS compania_aerea;
-DROP TABLE IF EXISTS empleado_en;
+DROP TABLE IF EXISTS persona CASCADE;
+DROP TABLE IF EXISTS compania_aerea CASCADE;
+DROP TABLE IF EXISTS empleado_en CASCADE;
 
-DROP TABLE IF EXISTS tripulacion_en;
-DROP TABLE IF EXISTS piloto_en;
-DROP TABLE IF EXISTS copiloto_en;
-DROP TABLE IF EXISTS licencia_piloto;
+DROP TABLE IF EXISTS tripulacion_en CASCADE;
+DROP TABLE IF EXISTS piloto_en CASCADE;
+DROP TABLE IF EXISTS copiloto_en CASCADE;
+DROP TABLE IF EXISTS licencia_piloto CASCADE;
 
-DROP TABLE IF EXISTS cliente;
-DROP TABLE IF EXISTS reserva;
-DROP TABLE IF EXISTS ticket;
+DROP TABLE IF EXISTS cliente CASCADE;
+DROP TABLE IF EXISTS reserva CASCADE;
+DROP TABLE IF EXISTS ticket CASCADE;
 
-DROP TABLE IF EXISTS vuelo;
-DROP TABLE IF EXISTS aerodromo;
-DROP TABLE IF EXISTS ciudad;
-DROP TABLE IF EXISTS punto;
-DROP TABLE IF EXISTS ruta;
-DROP TABLE IF EXISTS aeronave;
-DROP TABLE IF EXISTS costo;
+DROP TABLE IF EXISTS vuelo CASCADE;
+DROP TABLE IF EXISTS aerodromo CASCADE;
+DROP TABLE IF EXISTS ciudad CASCADE;
+DROP TABLE IF EXISTS punto CASCADE;
+DROP TABLE IF EXISTS ruta CASCADE;
+DROP TABLE IF EXISTS aeronave CASCADE;
+DROP TABLE IF EXISTS costo CASCADE;
 
 
 CREATE TABLE persona(
@@ -72,15 +72,15 @@ CREATE TABLE cliente(
 
 CREATE TABLE reserva(
 	id serial PRIMARY KEY,
-	comprador_id integer NOT NULL,
+	cliente_id integer,
 	codigo varchar UNIQUE NOT NULL
 );
 
 CREATE TABLE ticket(
 	numero serial PRIMARY KEY,
-	reserva_id integer NOT NULL,
-	vuelo_id integer NOT NULL,
-	persona_id integer NOT NULL,
+	reserva_id integer,
+	vuelo_id integer,
+	persona_id integer,
 	numero_asiento varchar NOT NULL, -- REVISAR tipo
 	clase varchar NOT NULL,
 	comida_y_maleta boolean NOT NULL
@@ -90,11 +90,11 @@ CREATE TABLE ticket(
 
 CREATE TABLE vuelo(
 	id serial PRIMARY KEY,
-	aerodromo_salida_id integer NOT NULL,
-	aerodromo_llegada_id integer NOT NULL,
-	ruta_id integer NOT NULL,
-	codigo_aeronave char(7) NOT NULL,
-	codigo_compania char(3) NOT NULL,
+	aerodromo_salida_id integer,
+	aerodromo_llegada_id integer,
+	ruta_id integer,
+	aeronave_codigo char(7),
+	compania_codigo char(3),
 	fecha_salida timestamp NOT NULL,
 	fecha_llegada timestamp NOT NULL,
 	velocidad real NOT NULL,
@@ -104,7 +104,7 @@ CREATE TABLE vuelo(
 
 CREATE TABLE aerodromo(
 	id serial PRIMARY KEY,
-	ciudad_id integer NOT NULL,
+	ciudad_id integer,
 	nombre varchar NOT NULL,
 	ICAO char(4) UNIQUE NOT NULL,
 	IATA char(3) UNIQUE NOT NULL,
@@ -124,8 +124,8 @@ CREATE TABLE ruta(
 );
 
 CREATE TABLE punto(
-	ruta_id integer NOT NULL,
-	cardinalidad integer NOT NULL,
+	ruta_id integer,
+	cardinalidad integer,
 	nombre varchar NOT NULL,
 	latitud real NOT NULL,
 	longitud real NOT NULL,
@@ -167,3 +167,36 @@ CREATE TABLE costo(
 \copy punto from './data/punto.csv' DELIMITER ',' CSV HEADER;
 \copy aeronave from './data/aeronave.csv' DELIMITER ',' CSV HEADER;
 \copy costo from './data/costo.csv' DELIMITER ',' CSV HEADER;
+
+
+ALTER TABLE empleado_en ADD FOREIGN KEY (trabajador_id) REFERENCES persona;
+ALTER TABLE empleado_en ADD FOREIGN KEY (compania_codigo) REFERENCES compania_aerea;
+
+ALTER TABLE tripulacion_en ADD FOREIGN KEY (trabajador_id) REFERENCES persona;
+ALTER TABLE tripulacion_en ADD FOREIGN KEY (vuelo_id) REFERENCES vuelo;
+
+ALTER TABLE piloto_en ADD FOREIGN KEY (trabajador_id) REFERENCES persona;
+ALTER TABLE piloto_en ADD FOREIGN KEY (vuelo_id) REFERENCES vuelo;
+
+ALTER TABLE copiloto_en ADD FOREIGN KEY (trabajador_id) REFERENCES persona;
+ALTER TABLE copiloto_en ADD FOREIGN KEY (vuelo_id) REFERENCES vuelo;
+
+ALTER TABLE licencia_piloto ADD FOREIGN KEY (piloto_id) REFERENCES persona;
+-- Como no existe en el enunciado de esta entrega, va a quedar comentado.
+-- ALTER TABLE liciencia_piloto ADD FOREIGN KEY (piloto_id) REFERENCES licensia;
+
+ALTER TABLE reserva ADD FOREIGN KEY (cliente_id) REFERENCES cliente;
+
+ALTER TABLE ticket ADD FOREIGN KEY (reserva_id) REFERENCES reserva;
+ALTER TABLE ticket ADD FOREIGN KEY (vuelo_id) REFERENCES vuelo;
+ALTER TABLE ticket ADD FOREIGN KEY (persona_id) REFERENCES persona;
+
+ALTER TABLE vuelo ADD FOREIGN KEY (aerodromo_salida_id) REFERENCES aerodromo;
+ALTER TABLE vuelo ADD FOREIGN KEY (aerodromo_llegada_id) REFERENCES aerodromo;
+ALTER TABLE vuelo ADD FOREIGN KEY (ruta_id) REFERENCES ruta;
+ALTER TABLE vuelo ADD FOREIGN KEY (aeronave_codigo) REFERENCES aeronave;
+ALTER TABLE vuelo ADD FOREIGN KEY (compania_codigo) REFERENCES compania_aerea;
+
+ALTER TABLE aerodromo ADD FOREIGN KEY (ciudad_id) REFERENCES ciudad;
+
+ALTER TABLE punto ADD FOREIGN KEY (ruta_id) REFERENCES ruta;
