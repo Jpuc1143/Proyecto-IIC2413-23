fbd6b1a579ae9756ada8f75690b8e703a15ef925
Jaime <jaime.perez@uc.cl>
2022-05-17 21:33:25 -0400

    Implemetado query basico para reservas

---
+++ Sites/aerolineas.php
@@ -0,0 +1,6 @@
+<h1>Listado de Aerolineas</h1>
+
+<?php
+	require("connection.php");
+
+	$query = "SELECT
--- Sites/connection.php
+++ Sites/connection.php
@@ -1,7 +1,7 @@
 <?php
 	try {
 		$db_name = "grupo23e2";
-		$user = "RinkuFry";
+		$user = "grupo23";
 		$password = "RinkuFry";
 		$db = new PDO("pgsql:dbname=$db_name;host=localhost;port=5432;user=$user;password=$password");
 	} catch (Exception $e) {
