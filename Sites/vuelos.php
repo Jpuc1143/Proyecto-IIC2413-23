ce3544ff642fb02c8e7ca440d1f5884ec6cbd068
BFry712 <bfry@uc.cl>
2022-05-18 00:33:46 -0400

    actualizacion vuelos.php

--- Sites/vuelos.php
+++ Sites/vuelos.php
@@ -6,23 +6,34 @@
 <body>
     <h1>Vuelos</h1>
     <hr>
-    <p> Primero seleccione el estado del vuelo que desea visualizar para los vuelos:</p>
-<form action="action_page.php" method="GET">
-  <input type="radio" id="Aceptado" name="estado_vuelo" value="Aceptado">
-  <label for="aceptado">Aceptado</label><br>
-  <input type="radio" id="Borrado" name="estado_vuelo" value="Borrado">
-  <label for="borrado">Borrado</label><br>
-  <input type="radio" id="Publicado" name="estado_vuelo" value="Publicado">
-  <label for="publicado">Publicado</label>
-  <input type="radio" id="Pendiente" name="estado_vuelo" value="Pendiente">
-  <label for="pendiente">Pendiente</label>
-</form
+<form method="GET">
+<label> Primero seleccione el estado del vuelo que desea visualizar:</label>
+<br>
+<input type="radio" id="Aceptado" name="estado_vuelo" value="Aceptado">
+<label for="aceptado">Aceptado</label><br>
+<input type="radio" id="Borrado" name="estado_vuelo" value="Borrado">
+<label for="borrado">Borrado</label><br>
+<input type="radio" id="Publicado" name="estado_vuelo" value="Publicado">
+<label for="publicado">Publicado</label>
+<br>
+<input type="radio" id="Pendiente" name="estado_vuelo" value="Pendiente">
+<label for="pendiente">Pendiente</label>
+<br>
+<input type="submit" value="Submit">
+</form>
+<?php 
+        $data = array(array(98,39,71,213,'TPUYDBP','COG','2022-04-21 13:35:00','2022-04-22 12:11:00',275,12577,'publicado'),array(99,56,50,263,'ZQZKYVL','QAF','2022-04-30 13:20:00','2022-05-02 12:00:00',275,12577,'pendiente')); 
+    ?> 
 <?php
 if (isset($_GET["estado_vuelo"])) {
-  if ($_GET["estado_vuelo"] == "Aceptado") {
-    echo "Aceptado";
+  if ($_GET["estado_vuelo"] == "Pendiente") {
+    for(;$data;) {
+      if ($data[i][0] == 'pendiente'); {
+        echo $data[i][10];
+      }
+    }
   } else {
-  echo "No Aceptado";
+  echo "No hay vuelos pendientes de ser aprobados";
 }
 }
 ?>
@@ -43,9 +54,6 @@ p {
   font-size: 15px;
 }
 </style>
-<?php 
-        $data = array(array(45,43,'Amsterdam'),array(46,42,'New York')); 
-        echo $data[0][2]; 
-    ?> 
 
 </html>
+
