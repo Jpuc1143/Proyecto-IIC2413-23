eb72a4f2446a39857c71adeae85a8150a01ecd08
Jaime <jaime.perez@uc.cl>
2022-05-17 21:43:59 -0400

    Implementado busqueda de reserva

--- Sites/reservas.php
+++ Sites/reservas.php
@@ -8,9 +8,15 @@
 <?php
 	require("connection.php");
 	
-	$query = "SELECT reserva.id,reserva.codigo, comprador.nombre, pasajero.nombre, vuelo.codigo, ticket.clase, ticket.numero_asiento, costo.valor FROM reserva JOIN ticket ON reserva.id = ticket.reserva_id JOIN vuelo ON ticket.vuelo_id = vuelo.id JOIN aeronave ON vuelo.aeronave_codigo = aeronave.codigo JOIN costo ON aeronave.peso = costo.peso AND vuelo.ruta_id = costo.ruta_id JOIN persona AS pasajero ON ticket.persona_id = pasajero.id JOIN persona AS comprador ON reserva.cliente_id = comprador.id ORDER BY reserva.id;";
+	$query = "SELECT reserva.id,reserva.codigo, comprador.nombre, pasajero.nombre, vuelo.codigo, ticket.clase, ticket.numero_asiento, costo.valor FROM reserva JOIN ticket ON reserva.id = ticket.reserva_id JOIN vuelo ON ticket.vuelo_id = vuelo.id JOIN aeronave ON vuelo.aeronave_codigo = aeronave.codigo JOIN costo ON aeronave.peso = costo.peso AND vuelo.ruta_id = costo.ruta_id JOIN persona AS pasajero ON ticket.persona_id = pasajero.id JOIN persona AS comprador ON reserva.cliente_id = comprador.id WHERE reserva.codigo ILIKE :codigo ORDER BY reserva.id;";
 	//$query = "SELECT * FROM aerodromo;";
 	$result = $db -> prepare($query);
+
+	$codigo = "%";
+	if (isset($_GET["codigo"])) {
+		$codigo = "%".$_GET["codigo"]."%";
+	}
+	$result -> bindParam("codigo", $codigo);
         $result -> execute();
 	$data = $result -> fetchAll();
 	
