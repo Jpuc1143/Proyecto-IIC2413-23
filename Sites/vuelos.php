df0aec4ecc992f086e5971ee276ff6ab5b6bf31b
BFry712 <bfry@uc.cl>
2022-05-20 14:53:10 -0400

    actualizacion vuelos

--- Sites/vuelos.php
+++ Sites/vuelos.php
@@ -10,20 +10,27 @@
 </div>
 <hr>
 <?php 
-        $data = array(array(98,39,71,213,'TPUYDBP','COG','2022-04-21 13:35:00','2022-04-22 12:11:00',275,12577,'publicado'),array(99,56,50,263,'ZQZKYVL','QAF','2022-04-30 13:20:00','2022-05-02 12:00:00',275,12577,'pendiente')); 
+        $data = array(array(98,39,71,213,'TPUYDBP','COG','2022-04-21 13:35:00','2022-04-22 12:11:00',275,12577,'publicado','XLE1416'),array(99,56,50,263,'ZQZKYVL','QAF','2022-04-30 13:20:00','2022-05-02 12:00:00',275,12577,'pendiente','QAF8273'));
+        $data2 = array(array(98,39,71,213,'TPUYDBP','COG','2022-04-21 13:35:00','2022-04-22 12:11:00',275,12577,'publicado','XLE1416',71,'Miami','La Cascada','IC34','34E','Latitud','Longitud','TPUYDBP','Compañia 1'),array(99,56,50,263,'ZQZKYVL','QAF','2022-04-30 13:20:00','2022-05-02 12:00:00',275,12577,'pendiente','QAF8273',50,'Chile','Arturo','IC44','34U','Latitud','Longitud','ZQZKYVL','Compañia 2')) 
     ?>
 
 <div align="center">
   <p> A continuación puede buscar los vuelos pendientes de ser aprobados por la DGAC</p>
-<button onclick="VuelosPendientes()"style="background-color:white;color:black;border-radius:5px">Buscar vuelos pendientes</button>
-<p id="pendiente"> </p>
-<script>
-var data = array(array(98,39,71,213,'TPUYDBP','COG','2022-04-21 13:35:00','2022-04-22 12:11:00',275,12577,'publicado'),array(99,56,50,263,'ZQZKYVL','QAF','2022-04-30 13:20:00','2022-05-02 12:00:00',275,12577,'pendiente'))
-  function VuelosPendientes(){
-    document.getElementById("pendiente").innerHTML = data[0][0]
-  }
-</script>
+<form>
+  <input type="submit" name="vuelos_pendientes" value="Buscar Vuelos Pendientes">
+</form>
 </div>
+<?php
+if (isset($_GET["vuelos_pendientes"])) {
+  if ($_GET["vuelos_pendientes"] == "Buscar Vuelos Pendientes") {
+    foreach($data as $arreglo) {
+      if ($arreglo[10] == "pendiente"){
+        echo "<tr> <td> $arreglo[0]</td> <td>$arreglo[11]</td></tr><br>";
+      }
+    }
+  }
+}
+?>
 <hr>
 <div align="center">
   <p> A continuación puede buscar los vuelos que son aceptados por cierta aerolínea y que tienen como destino un aeródromo con cierto código ICAO específico:</p>
@@ -32,26 +39,22 @@ var data = array(array(98,39,71,213,'TPUYDBP','COG','2022-04-21 13:35:00','2022-
   <input type="text" id="icao" name="icao" value=""><br>
   <label for="aerolinea">Aerolínea:</label><br>
   <input type="text" id="aerolinea" name="aerolinea" value=""><br><br>
-  <input type="submit" value="Submit">
+  <input type="submit" name ="boton_submit" value="Submit">
 </form> 
 </div>
-<hr>
-<div align="center">
-<button onclick="PagPrincipal_Return()"style="background-color:white;color:black;border-radius:5px">Regresar a la página principal</button>
-</div>
 <?php
-if (isset($_GET["submit"])) {
-  if ($_GET["submit"] == "Vuelos Pendientes") {
-    for($i=0;$i!=$data.length;$i++) {
-      if ($data[i][0] == 'pendiente'); {
-       echo $data[i][10];
+if (isset($_GET["boton_submit"])) {
+  if (isset($_GET["icao"])){
+    if (isset($_GET["aerolinea"])){
+      foreach($data2 as $arreglo){
+        if ($_GET["icao"]==$arreglo[15] AND $_GET["aerolinea"]==$arreglo[20]){
+          echo "<tr> <td> $arreglo[0]</td> <td>$arreglo[11]</td></tr><br>"; 
+        }
       }
     }
-  } 
-  else {
-  echo "No hay vuelos pendientes de ser aprobados";
+  }
 }
-}
+
 ?>
 </body>
 <style>
