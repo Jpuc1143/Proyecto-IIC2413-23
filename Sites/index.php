7c74b66be047da9c46f8a05276b1c9aef271c1a8
Jaime <jaime.perez@uc.cl>
2022-05-21 17:03:46 -0400

    Implementado render generico de tablas

--- Sites/index.php
+++ Sites/index.php
@@ -2,4 +2,20 @@
 
 <h2>Index</h2>
 
+<?php
+
+include("queries/query5.php");
+if (isset($data)) {
+	echo "<table>";
+	foreach($data as $row) {
+		echo "<tr>";
+		foreach($row as $cell) {
+			echo "<td>$cell</td>";
+		}
+		echo "</tr>";
+	}
+	echo "</table>";
+}
+?>
+
 <?php include("footer.html") ?>
