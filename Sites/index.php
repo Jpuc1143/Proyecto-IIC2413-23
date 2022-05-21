9d7b6451e60bb794131fae70925df3a73026728f
Jaime <jaime.perez@uc.cl>
2022-05-21 18:52:17 -0400

    Implementado queries 3 y 4

--- Sites/index.php
+++ Sites/index.php
@@ -3,8 +3,19 @@
 <h2>Index</h2>
 
 <form>
+        <label>Ingrese su código de reserva:</label>
+        <input type="text" name="codigo">
+	<button type="submit" name="query" value="3">Buscar</button>
+</form>
+
+<form>
+	<label>Clientes Frequentes</label>
+	<button type="submit" name="query" value="4">Buscar</button>
+</form>
+
+<form>
 	<label>Estados de vuelos por nombre de aerolinea</label></br>
-	<label>Aerolinea: </label>
+	<label>Nombre aerolinea: </label>
 	<input type="text" name="compania">
 	<button type="submit" name="query" value="5">Buscar</button>
 </form>
@@ -22,8 +33,10 @@ if (isset($_GET["query"])) {
 		case "2":
 			break;
 		case "3":
+			include("queries/query3.php");
 			break;
 		case "4":
+			include("queries/query4.php");
 			break;
 		case "5":
 			include("queries/query5.php");
